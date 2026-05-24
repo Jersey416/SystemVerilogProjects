3530d9b2e0b3958adf3db1a79c8f867fb3f78f16
Jake Ryan <jdryan02@GameStation.localdomain>
2026-05-23 23:34:42 -0400

    created add and sub, worked on structure and layout

--- RISC-V_CPU/alu.sv
+++ RISC-V_CPU/alu.sv
@@ -1,31 +1,109 @@
 
-import modes::alu_mode;
+import alu::*;
 
 module alu( 
     input logic clk, 
     input logic[31:0] rd, 
     input logic[31:0] rs1, 
     input logic[31:0] rs2_imm,
-    input logic[3:0]  flags
+    input alu_flags_t alu_flags;
 );
 
-return rs1+rs2;
+typedef struct packed{
+    logic[31:0] result;
+    alu_flags_t flags;
+} alu_return_t;
+
+function alu_return_t and ; endfunction
+function alu_return_t or  ; endfunction
+function alu_return_t not ; endfunction
+function alu_return_t nand; endfunction
+function alu_return_t nor ; endfunction
+function alu_return_t xor ; endfunction
+function alu_return_t add ; endfunction
+function alu_return_t sub ; endfunction
+function alu_return_t mul ; endfunction
+function alu_return_t div ; endfunction
+function alu_return_t lsl ; endfunction
+function alu_return_t lsr ; endfunction
+function alu_return_t asl ; endfunction
+function alu_return_t asr ; endfunction
+function alu_return_t foo0; endfunction
+function alu_return_t foo1; endfunction
+
 
 always_ff @(posedge clk) begin
     
-    
 end
 
-function logic [32:0] add(
-    logic[31:0] rs1, 
-    logic[31:0] rs2
+
+function alu_return_t and(
+    logic[31:0] rs1, rs2,
+    alu_flags_t curr_flags
+);  
+    logic[31:0] tmp = rs1 & rs2;
+    
+    return
+endfunction
+
+function alu_return_t or(
+    logic[31:0] rs1, rs2
+); 
+    
+    return
+endfunction
+
+function alu_return_t not(
+    logic[31:0] rs1, rs2
+); 
+    
+    return
+endfunction
+
+function alu_return_t nan(
+    logic[31:0] rs1, rs2
+); 
+    
+    return
+endfunction
+
+function alu_return_t nor(
+    logic[31:0] rs1, rs2
+); 
+    
+    return
+endfunction
+
+function alu_return_t xor(
+    logic[31:0] rs1, rs2
+); 
+    
+    return
+endfunction
+
+
+function alu_return_t add(      //32-b return + 4 bit flags
+    logic[31:0] rs1, rs2 
 );
-    return {1'b0, rs1} + {1'b0, rs2;};
-endfunction;
+    alu_flags_t flags = '0;
+    logic[32:0] tmp = {1'b0, rs1} + {1'b0, rs2};
 
-a
+        flags.n = tmp[31];                                      //if MSB active activate N flag
+        flags.c = tmp[32];                                      //if carry occured activate C flag
+        flags.v = rs1[31]==rs2[31]) && (rs1[31] != tmp[31]);    //if signed overflow occured activate V flag
+        flags.z = (tmp[31:0] == 32'b0);                         //if 0 activate Z flag
 
+    return {flags.n,flags.c,flags.v,flags.z,tmp[31:0]};
+
+endfunction
+
+function alu_return_t sub(
+    logic[31:0] rs1, rs2 
+);
+    return add(rs1, (~rs2 + 1'b1));                             //use 2's comp and add
+endfunction
 
 
 
 endmodule
+
--- RISC-V_CPU/packages.sv
+++ RISC-V_CPU/packages.sv
@@ -1,6 +1,14 @@
-package modes;
+package alu;
     typedef enum logic[3:0] {
         AND, OR, NOT, AND, OR, NAND, NOR, XOR,
         ADD, SUB, MUL, DIV, LSL, LSR, ASL, ASR 
-        } ;
+    } alu_running_mode;
+
+    typedef struct packed{
+        logic n;
+        logic c;
+        logic v;
+        logic z;
+    } alu_flags_t;
+
 endpackage
