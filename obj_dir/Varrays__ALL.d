447921aba717fa45707ad2e3c8b25bb43b4ad705
Jake Ryan <jdryan02@gmail.com>
2026-05-27 00:39:23 -0400

    removed obj_dir to avoid clutter

--- obj_dir/Varrays__ALL.d
+++
@@ -1,9 +0,0 @@
-Varrays__ALL.o: Varrays__ALL.cpp Varrays.cpp Varrays__pch.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Varrays__Syms.h \
- Varrays.h Varrays___024root.h Varrays___024root__0.cpp Varrays__main.cpp \
- Varrays___024root__Slow.cpp Varrays___024root__0__Slow.cpp \
- Varrays__Syms__Slow.cpp
