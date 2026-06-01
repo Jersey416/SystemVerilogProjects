bd1d4faf6f8567e6b966772bc51c1231fee3d188
Jake Ryan <jdryan02@gmail.com>
2026-06-01 00:47:58 -0400

    reg file working
    finished rf tb and verified working simulation

--- RISC-V_CPU/rf.sv
+++ RISC-V_CPU/rf.sv
@@ -1,7 +1,7 @@
 module rf(
     input logic clk,
     input logic rst,
-    input logic write_enable,
+    input logic wren,
     input rf_types::reg_addr_t waddr,
     input rf_types::reg_addr_t raddr0,
     input rf_types::reg_addr_t raddr1,
@@ -10,14 +10,18 @@ module rf(
     output global_defs::word_t rdata1
 );
 
+    import global_defs::*;
     import rf_types::*;
 
     word_t r[32];
 
-    always_ff @(posedge clk) begin
-        
+    always_comb begin               //combinational reads
+
         rdata1 = r[raddr1];
         rdata0 = r[raddr0];
+    end
+
+    always_ff @(posedge clk) begin  //sequential writes
         
         if(! rst) begin
             r <= '{default: '0};    
--- RISC-V_CPU/test_bench/rf_tb.sv
+++ RISC-V_CPU/test_bench/rf_tb.sv
@@ -1,11 +1,12 @@
 
 module rf_tb;
 
-    import globat_defs::*;
-    import  rf_types::*;
+    import global_defs::*;
+    import rf_types::*;
     
     logic clk, rst, wren;
-    word_t waddr,raddr0,raddr1,wdata,rdata0,rdata1;
+    reg_addr_t waddr,raddr0,raddr1;
+    word_t wdata,rdata0,rdata1;
 
     word_t rd0 = '0;               
     word_t rd1 = '0;
@@ -19,13 +20,14 @@ module rf_tb;
         .raddr1(raddr1),
         .wdata(wdata),
         .rdata0(rdata0),
-        .rdata1(rdata1),
+        .rdata1(rdata1)
     );
 
     always #5 clk = ~clk;
 
+
+
     task rf_test(
-        input logic clk,
         input logic rs,
         input logic we,
         input reg_addr_t wa,
@@ -35,31 +37,51 @@ module rf_tb;
     );
         
         rst = rs;
-        wren = re;
-
-        always_ff @(posedge clk); begin
-            
-            waddr <= wa;
-            raddr0 <= ra0;
-            raddr1 <= ra1;
-            wdata <= wd;
-            rd0 <= rdata0;
-            rd1 <= rdata1;
-            
-            $display("%s: %0h   %s: %0h   %s: %0h   %s: %0h", 
-                    waddr.name(), wdata, 
-                    raddr0.name(), rd0, 
-                    raddr1.name(), rd1);
-        end
+        wren = we;
+        waddr = wa;
+        raddr0 = ra0;
+        raddr1 = ra1;
+        wdata = wd;
+        
+        @(posedge clk);
+        
+        rd0 = rdata0;
+        rd1 = rdata1;
+        
+        $display("%s: %0h   %s: %0h   %s: %0h", 
+                waddr.name(), wdata, 
+                raddr0.name(), rd0, 
+                raddr1.name(), rd1);
 
     endtask
 
+    reg_addr_t i;
+
     initial begin
 
-        rf_test(clk, '0, '0, '0, '0, '0, '0);
-        rf_test(clk, '1, '1, T0, '0, '0, 32'hFEDC_BA98);
-        rf_test(clk, '1, '0, '0, '0, T0, T0);
+        rf_test('0, '0, ZERO, ZERO, ZERO, '0);
+        for (i = i.first(); i < i.last(); i = i.next())
+            rf_test('1, '1, i, ZERO, ZERO, 32'(i));
+        
+        rf_test('1, '1, T6, ZERO, ZERO, 32'h1f);
 
+
+        for (i = i.first(); i < i.last(); i = i.next())
+            rf_test('1, '0, ZERO, i, i.next(), '0);
+
+        rf_test('1, '0, ZERO, T6, ZERO, '0);
+        $finish;
     end
 
 endmodule
+
+/*
+        rf_test('0, '0, ZERO, ZERO, ZERO, '0);
+        rf_test('1, '1, T0,   ZERO, ZERO, 32'hFEDC_BA98);
+        rf_test('1, '0, ZERO, ZERO, T0,   '0);
+        rf_test('1, '0, ZERO, ZERO, T0,   '0);
+        rf_test('1, '1, T0,   ZERO, ZERO, 32'h7654_3210);
+        rf_test('1, '0, ZERO, ZERO, T0,   '0);
+        rf_test('1, '1, T1,   ZERO, ZERO, 32'hFEDC_BA98);
+        rf_test('1, '0, ZERO, T0,   T1,   '0);
+*/
